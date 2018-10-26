dfeaba21a76600eb406d550d74f69ca9c32b41d7
Michelle Felix <mich.feloz@gmail.com>
2018-10-26 03:02:52 -0500

    Comportamiento responsive en login (celulares)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,52 +14,65 @@ declare function init_plugins();
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
+  recordarPassword: boolean = false;
+  email: string;
 
-  recordarPassword:boolean=false;
-  email:string;
+  movil: boolean = false;
 
   constructor(
-    private _usuarioService:UsuarioService,
-    private router:Router,
-  ) {}
-
-  ngOnInit() {
-
-    init_plugins();
-
-    this.email = localStorage.getItem('email') || '';
-    if( this.email.length > 1 ){
-      this.recordarPassword = true;
-    }
-    
+    private _usuarioService: UsuarioService,
+    private router: Router
+  ) {
+    window.addEventListener("resize", ()=>{
+      this.validarMovil();
+    })
   }
 
-  login(forma:NgForm){
+  ngOnInit() {
+    init_plugins();
 
+    this.email = localStorage.getItem("email") || "";
+    if (this.email.length > 1) {
+      this.recordarPassword = true;
+    }
+
+    this.validarMovil();
+  }
+
+  validarMovil() {
+    if (window.innerWidth <= 600 && window.innerHeight <= 850) {
+      this.movil = true;
+    } else {
+      this.movil = false;
+    }
+  }
+
+  login(forma: NgForm) {
     //Validando que la forma sea válida
-    if( forma.invalid ){
-      return
+    if (forma.invalid) {
+      return;
     }
     //---->
 
     //Construyendo el objeto usuario con los valores del formulario
-    let usuario = new Usuario(null, forma.value.email, forma.value.password );
+    let usuario = new Usuario(null, forma.value.email, forma.value.password);
     //---->
 
     // Enviando los datos de acceso a través de nuestro servicio login y recibiendo
     // true o false dependiendo si el login fue exitoso o no
-    this._usuarioService.login( usuario, forma.value.recordarPassword ).
-          subscribe( (correcto)=>{
-            
-            if(correcto){
-              this.router.navigate(['/dashboard']);
-            }else{
-              swal('Login', 'El usuario o la contraseña son incorrectos', 'warning');
-            }
-            
-          });
+    this._usuarioService
+      .login(usuario, forma.value.recordarPassword)
+      .subscribe(correcto => {
+        if (correcto) {
+          this.router.navigate(["/dashboard"]);
+        } else {
+          swal(
+            "Login",
+            "El usuario o la contraseña son incorrectos",
+            "warning"
+          );
+        }
+      });
     //---->
-    
-
   }
 }

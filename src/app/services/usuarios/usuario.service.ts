90e2ff0e197332e237554b05df2385ba22e0dd0f
Michelle Felix <mich.feloz@gmail.com>
2018-10-26 00:23:38 -0500

    Metodos para servicio usuario

--- src/app/services/usuarios/usuario.service.ts
+++ src/app/services/usuarios/usuario.service.ts
@@ -2,49 +2,113 @@ import { Injectable } from '@angular/core';
 import { Usuario } from '../../models/usuario.model';
 import { HttpClient } from '@angular/common/http';
 import { URL_SERVICIOS } from '../../config/config';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UsuarioService {
+  usuario: Usuario;
+  id:string;
+  token: string;
 
-  constructor(
-    public http:HttpClient
-  ) { }
+  constructor(public http: HttpClient) {
+    this.cargarStorage();
+  }
+  //------------------------
+  // Función de login
+  //------------------------
+  login(usuario: Usuario, recordar: boolean = false) {
+    //Guardamos o borramos el email del local storage, segun las preferencias del usuario
+    if (recordar) {
+      localStorage.setItem("email", usuario.email);
+    } else {
+      localStorage.removeItem("email");
+    }
+    // ---->
 
-  login( usuario:Usuario, recordar:boolean = false ){
+    //Construimos ruta del servicio
+    let url = URL_SERVICIOS + "/login";
+    // ---->
 
-    if ( recordar ) {
-      localStorage.setItem( 'email', usuario.email );
+    // Realizamos petición de login al backend
+    return this.http.post(url, usuario).pipe(
+      map((resp: any) => {
+        // Guardamos los datos del usuario y su token
+        this.guardarStorage(resp.id, resp.token, resp.usuario);
+        return true;
+        // ---->
+      }),
+      catchError((err)=>{
+        return of(false);
+      })
+    );
+    // FIN de Petición al backend
+  }
+  //------------------------
+  // FIN de Función login
+  //------------------------
+
+  //-------------------------------------
+  // Función crear Usuario
+  //-------------------------------------
+  crearUsuario(usuario: Usuario) {
+    let url = URL_SERVICIOS + "/usuario";
+
+    return this.http.post(url, usuario);
+  }
+  //-------------------------------------
+  // FIN de Función crear Usuario
+  //-------------------------------------
+
+  //------------------------------------------------------------------
+  // Función para guardar datos del usuario en localstorage
+  //------------------------------------------------------------------
+  guardarStorage(id: string, token: string, usuario: Usuario) {
+    localStorage.setItem("id", id);
+    localStorage.setItem("token", token);
+    localStorage.setItem("usuario", JSON.stringify(usuario));
+
+    this.usuario = usuario;
+    this.token = token;
+  }
+  //------------------------------------------------------------------
+  // FIN de Función para guardar datos del usuario en localstorage
+  //------------------------------------------------------------------
+
+  //-------------------------------------------------------------------------
+  // Función para cargar los datos del usuario desde el localstorage
+  //-------------------------------------------------------------------------
+  cargarStorage() {
+    if(localStorage.getItem('token')){
+      this.token=localStorage.getItem('token');
+      this.usuario = JSON.parse(localStorage.getItem('usuario'));
+      this.id = localStorage.getItem('id');
     }else{
-      localStorage.removeItem('email');
+      this.token='';
+      this.usuario=null;
+      this.id='';
     }
 
-    let url = URL_SERVICIOS + '/login';
-
-    return this.http.post( url, usuario ).pipe( map( (resp:any)=>{
-      
-      localStorage.setItem('id', resp.id );
-      localStorage.setItem('token', resp.token);
-      localStorage.setItem( 'usuario', JSON.stringify(resp.usuario) );
-
-      
-
-      return true;
-
-    }));
-              
-
   }
+  //-------------------------------------------------------------------------
+  // FIN de Función para cargar los datos del usuario desde el localstorage
+  //-------------------------------------------------------------------------
 
-  crearUsuario( usuario:Usuario){
-
-    let url = URL_SERVICIOS + '/usuario';
-
-    return this.http.post( url, usuario );
+  //--------------------------------------------------------------
+  // Función para validar si el usuario esta logueado o no
+  //--------------------------------------------------------------
+  validarLogin() {
+    let url = URL_SERVICIOS + "/validarToken";
+    
+    return this.http.post(url,{
+      'token': this.token
+    });
   }
-
+  //--------------------------------------------------------------
+  // FIN Función para validar si el usuario esta logueado o no
+  //--------------------------------------------------------------
 }

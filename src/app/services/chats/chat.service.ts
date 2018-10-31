c50fab4487e1349fd80247b064e984e8c0f97a4e
Michelle Felix <mich.feloz@gmail.com>
2018-10-30 20:50:31 -0600

    Se agrega servicio para manejar chats

---
+++ src/app/services/chats/chat.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
+import { UsuarioService } from '../usuarios/usuario.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor(
+    private http:HttpClient,
+    private _usuarioService: UsuarioService
+  ) { }
+
+  guardarChat(chat, chatsCargados){
+    let token = this._usuarioService.token;
+    let url= URL_SERVICIOS + '/chat?token='+ token + '&chatCargados=' + chatsCargados;
+
+    return this.http.post( url , chat);
+    
+  }
+
+  obtenerChats(proyectoId, chatsCargados){
+    let token = this._usuarioService.token;
+    let url = URL_SERVICIOS + '/chat/' + proyectoId + '?token=' + token + '&chatsCargados=' + chatsCargados;
+    
+    return this.http.get( url );
+  
+  
+  }
+
+}

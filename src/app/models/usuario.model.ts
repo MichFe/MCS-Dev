491e6295d55c1c331e4b714862cbd3282ff92ad5
Michelle Felix <mich.feloz@gmail.com>
2019-01-07 14:52:30 -0600

    Se agrega propiedad unidadDeNegocio

--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -4,6 +4,7 @@ export class Usuario{
         public nombre: string,
         public email: string,
         public password: string,
+        public unidadDeNegocio: string,
         public role?:string,
         public img?: string,
         public _id?: string,

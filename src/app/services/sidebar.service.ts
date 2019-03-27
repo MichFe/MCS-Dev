2156d96fd0eb832a204247e9dd564d13a5b5b85a
Michelle Felix <mich.feloz@gmail.com>
2019-03-26 21:07:46 -0600

    Se agrega modulo compras y ajusta el menu

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { RequisicionesService } from './requisiciones/requisiciones.service';
 
 @Injectable({
   providedIn: "root"
 })
 export class SidebarService {
+
+  //Menu
   menu: any = [
     {
       modulo: "Vender",
@@ -20,8 +23,41 @@ export class SidebarService {
         { titulo: "Catálogo de Productos", url: "/catalogoProductos" },
         { titulo: "Cuentas por cobrar", url: "/cuentasPorCobrar" }
       ]
+    },
+    {
+      modulo: "Compras",
+      icono: "mdi mdi-tag-text-outline",
+      url:"/requisiciones",
+      submenu:[
+        { titulo: "Requisiciones", url: "/requisiciones" },
+        { titulo: "Aprobaciones", url: "/aprobaciones" },
+        { titulo: "Compras", url:"/compras" },
+        { titulo: "Cuentas por pagar", url: "/cuentasPorPagar"}
+      ]
     }
   ];
 
-  constructor() {}
+  constructor( 
+    private _requisicionesService:RequisicionesService
+   ) {}
+
+  obtenerTotalRequisicionesPorAprobar(){
+
+    this._requisicionesService.obtenerRequisicionesPorAprobar(1)
+      .subscribe(
+        (resp: any) => {
+          this.menu.forEach(menu => {
+            if (menu.modulo == 'Compras') {
+              menu.submenu.forEach(submenu => {
+                if (submenu.titulo == 'Aprobaciones') {
+                  submenu.notificacion = resp.totalReqisiciones;
+                }
+              });
+            }
+          });
+        });
+
+  }
+
+  
 }

6fed24cce158a383782eb18d123c9f89a8cdedac
Michelle Felix <mich.feloz@gmail.com>
2019-03-26 20:56:12 -0600

    Se agrega propiedad solicitante al modelo requisicion

---
+++ src/app/models/requisicion.model.ts
@@ -0,0 +1,21 @@
+import {Usuario} from './usuario.model';
+import {EstatusRequisicion} from '../enums/estatusRequisicion.enum';
+
+export class Requisicion {
+
+    constructor(
+       public descripcion?: string,
+       public cantidad?: string,
+       public solicitante?: Usuario,
+       public unidadDeNegocio?: string,
+       public fechaSolicitud?: Date,
+       public _id?: string,
+       public estatus?: EstatusRequisicion,
+       public aprobador?: string,
+       public fechaAprobacionRechazo?: Date,
+       public compraCreada?: boolean,
+       public productoRecibido?: boolean
+    ) { }
+
+
+}

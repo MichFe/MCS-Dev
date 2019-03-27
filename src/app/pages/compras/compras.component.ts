d8952e12ea29be6bb012b73003e71f0cd826abce
Michelle Felix <mich.feloz@gmail.com>
2019-03-27 17:37:54 -0600

    Se agrega tabla: todas las compras

--- src/app/pages/compras/compras.component.ts
+++ src/app/pages/compras/compras.component.ts
@@ -24,11 +24,21 @@ export class ComprasComponent implements OnInit {
   public currentPage = 1;
   public totalElementos = 0;
   totalCompras: number;
+  totalTodasLasCompras:number;
   compra:any;
 
-  //Variables Paginao de Compras
+  //Variables Paginado de Compras
   paginaActualCompras:number;
   paginasCompras:any[]=[
+    {
+      pagina: 1,
+      active: false
+    }
+  ];
+
+  //Variables Paginado de Compras
+  paginaActualTodasLasCompras: number;
+  paginasTodasLasCompras: any[] = [
     {
       pagina: 1,
       active: false
@@ -38,6 +48,7 @@ export class ComprasComponent implements OnInit {
   //Data
   requisicionesAprobadas: Requisicion[];
   compras:any[]=[];
+  todasLasCompras:any[]=[];
   
 
   // Paginado
@@ -58,12 +69,14 @@ export class ComprasComponent implements OnInit {
   ngOnInit() {
     this.obtenerRequisicionesAprobadas(1);
     this.obtenerCompras(1);
+    this.obtenerTodasLasCompras(1);
   
   }
 
   actualizarData(event){
     this.obtenerRequisicionesAprobadas(1);
     this.obtenerCompras(1);
+    this.obtenerTodasLasCompras(1);
   }
 
   registrarCliente(){
@@ -86,6 +99,59 @@ export class ComprasComponent implements OnInit {
             'error'
           );
         });
+  }
+
+  obtenerTodasLasCompras(pagina:number){
+    let desde = (pagina -1)*10;
+
+    this._comprasService.obtenerCompras(desde, false)
+      .subscribe(
+        (resp:any)=>{
+          this.todasLasCompras = resp.compras;
+          this.totalTodasLasCompras = resp.totalCompras;
+          this.paginarResultadosTodasLasCompras();
+          this.activarPaginaActualTodasLasCompras(pagina);
+      },
+      (error)=>{
+        swal(
+          'Error al consultar las compras',
+          error.error.mensaje + ' | ' + error.error.errors.message,
+          'error'
+        );
+      });
+  }
+
+  activarPaginaActualTodasLasCompras(paginaClickeada:number){
+    this.paginasTodasLasCompras.forEach(pagina => {
+      if (paginaClickeada === pagina.pagina) {
+        pagina.active = true;
+        this.paginaActualTodasLasCompras = pagina.pagina;
+      } else {
+        pagina.active = false;
+      }
+    });
+  }
+
+  paginarResultadosTodasLasCompras(){
+    if (!this.totalTodasLasCompras) {
+      return;
+    }
+
+    this.paginasTodasLasCompras = [];
+    const numeroDePaginas = Math.ceil(this.totalTodasLasCompras / 10);
+    let objetoPagina;
+
+    for (let pagina = 1; pagina <= numeroDePaginas; pagina++) {
+      objetoPagina = {
+        pagina: pagina,
+        active: false
+      };
+
+      this.paginasTodasLasCompras.push(objetoPagina);
+
+    }
+
+    this.paginasTodasLasCompras[0].active = true;
   }
 
   obtenerCompras(pagina: number) {
@@ -118,6 +184,11 @@ export class ComprasComponent implements OnInit {
   cargarElementosPaginaCompras(pagina) {
     this.obtenerCompras(pagina);
     this.activarPaginaActualCompras(pagina);
+  }
+
+  cargarElementosPaginaTodasLasCompras(pagina) {
+    this.obtenerTodasLasCompras(pagina);
+    this.activarPaginaActualTodasLasCompras(pagina);
   }
 
 
@@ -165,6 +236,17 @@ export class ComprasComponent implements OnInit {
     this.cargarElementosPaginaCompras(paginaActual.pagina - 1);
   }
 
+  paginaAnteriorTodasLasCompras() {
+    const paginaActual = this.paginasTodasLasCompras.find(pagina => {
+      return pagina.active;
+    });
+
+    if (paginaActual.pagina === 1) {
+      return;
+    }
+    this.cargarElementosPaginaTodasLasCompras(paginaActual.pagina - 1);
+  }
+
   paginaSiguiente() {
     const paginaActual = this.paginas.find(pagina => {
       return pagina.active;
@@ -185,6 +267,17 @@ export class ComprasComponent implements OnInit {
       return;
     }
     this.cargarElementosPaginaCompras(paginaActual.pagina + 1);
+  }
+
+  paginaSiguienteTodasLasCompras() {
+    const paginaActual = this.paginasTodasLasCompras.find(pagina => {
+      return pagina.active;
+    });
+
+    if (paginaActual.pagina * 10 >= this.totalTodasLasCompras) {
+      return;
+    }
+    this.cargarElementosPaginaTodasLasCompras(paginaActual.pagina + 1);
   }
 
   paginarResultadosCompras(){
